d80f864ae4cb4590d16a6c3907695171e1c3a116
David Bilge <david.bilge@viaprinto.de>
2013-03-13 14:03:53 +0100

    Added popout menu directive

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,1 +1,35 @@
+function calculatorController($scope) {
 
+}
+
+angular.module('calculator.directive', [])
+    .directive('popoutTrigger', function() {
+        return {
+                link: function(scope, element, attrs) {
+                element.bind('click', toggle);
+
+                scope.visible = null;
+
+                function toggle() {
+                    if (scope.visible != null) {
+                        scope.visible.removeClass('popout-open');
+                        scope.visible.addClass('popout-closed');
+                    }
+
+                    var thisPopout = element.siblings('.popout');
+
+                    if (scope.visible != null && scope.visible[0] == thisPopout[0]) {
+                        scope.visible = null;
+                    } else {
+                        scope.visible = thisPopout;
+                        scope.visible.removeClass('popout-closed');
+                        scope.visible.addClass('popout-open');
+                    }
+
+                }
+            }
+        };
+
+    });
+
+angular.module('calculator', ['calculator.directive']);
