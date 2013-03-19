5539043adf991494dffaa30f16a73ba79c513df2
David Bilge <david.bilge@viaprinto.de>
2013-03-19 09:07:49 +0100

    Added i18n support

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,4 +1,4 @@
-function calculatorController($scope, $window, Products) {
+function calculatorController($scope, $http, Products) {
     $scope.openPopoutMenu = '';
 
     $scope.setOpenPopoutMenu = function (id) {
@@ -22,11 +22,40 @@ function calculatorController($scope, $window, Products) {
 
 angular.module('calculator.directive', []);
 
+angular.module('calculator.filter', ['calculator.services']).filter('i18n', function(I18n) {
+    return function(input) {
+        return I18n.translate(input);
+    };
+});
+
 angular.module('calculator.services', ['ngResource']).
     factory('Products', function ($resource) {
         return $resource('http://localhost\\:8080/resttest/api/products/:productName/', {}, {
             query: {method: 'GET', params: {productName: '@name'}, isArray: true}
         });
+    }).
+    factory('I18n', function ($http) {
+        var internationalize = {
+            dictionary: undefined,
+            loadDictionary: function loadDictionary(iso_lang) {
+                $http.get('i18n/' + iso_lang + ".json").success(function(data) {
+                    internationalize.dictionary = data;
+                });
+            },
+            translate: function translate(key) {
+                if (typeof internationalize.dictionary === "object" && key in internationalize.dictionary) {
+                    return internationalize.dictionary[key];
+                } else {
+                    // TODO: Push error to server
+
+                    return 'i18nError: ' + key;
+                }
+            }
+        };
+
+        internationalize.loadDictionary('de_de');
+
+        return internationalize;
     });
 
 angular.module('calculator.routers', [])
@@ -37,4 +66,4 @@ angular.module('calculator.routers', [])
             .otherwise({redirectTo: '/products/booklet'});
     });
 
-angular.module('calculator', ['calculator.directive', 'calculator.services']);
+angular.module('calculator', ['calculator.directive', 'calculator.services', 'calculator.filter']);
